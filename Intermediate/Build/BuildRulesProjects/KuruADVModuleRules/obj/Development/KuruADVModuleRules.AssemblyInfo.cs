3f675ae3efb24a8bc870a4f9635d63fb728d1882
EmiteInna <3280788276@qq.com>
2024-09-16 20:40:38 +0800

    孩子们，我回来了

--- Intermediate/Build/BuildRulesProjects/KuruADVModuleRules/obj/Development/KuruADVModuleRules.AssemblyInfo.cs
+++ Intermediate/Build/BuildRulesProjects/KuruADVModuleRules/obj/Development/KuruADVModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KuruADVModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e785a92aa2d3a48e75d7cf84e3a888cb5666a871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bc6c326193e4bf24d5b93c13fad8cca4a56cd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("KuruADVModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KuruADVModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
